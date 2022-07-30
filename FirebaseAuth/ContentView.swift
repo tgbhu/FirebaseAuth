cf04f23314b4e0cf15345df4f7f8278e538c1879
tamaspinter <tamas.pinter@live.com>
2022-07-30 15:10:25 +0200

    Initial Commit

---
+++ FirebaseAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseAuth
+//
+//  Created by Tamás Pintér on 2022. 07. 30..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseAuth/FirebaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthApp.swift
+//  FirebaseAuth
+//
+//  Created by Tamás Pintér on 2022. 07. 30..
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
